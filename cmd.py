0df7f71939aa1dc5a35989c23f09f544ef17f32e
natelincyber <60009202+natelincyber@users.noreply.github.com>
2020-08-22 18:51:54 -0700

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -4,7 +4,7 @@ import sys
 import random
 
 
-insultlist = ['By popular demand your code is in /dev/null/', 'Your code is so bad, your child processes disowned you', 'Your mom circulates like a pulic key, seving more requests that HTTP', 'God must have had a broken GPU when he made you', 'This is the goverments plan to see if monkeys can type. And I have to say, they successfully accomplished that.', 
+insultlist = ['By popular demand your code is in /dev/null/', 'Your code is so bad, your child processes disowned you', 'Your mom circulates like a public key, serving more requests than HTTP', 'God must have had a broken GPU when he made you', "This is the government's plan to see if monkeys can type. And I have to say, they successfully accomplished that.", 
              'N00b', 'I never believed in chaos theory until I saw your code!']
 
 initcommand = sys.argv[1]
